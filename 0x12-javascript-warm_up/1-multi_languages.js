38600d273439022d05fdaccfa27ea95f40867707
Audraaires <sbansah360@gmail.com>
2023-04-19 10:33:19 +0000

    Javascript warmup

--- 0x12-javascript-warm_up/1-multi_languages.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/node
-// script to print out on a console usin console.log
-console.log('C is fun\nPython is cool\nJavaScript is amazing');
--- 0x12-javascript-warm_up/README.md
+++ 0x12-javascript-warm_up/README.md
@@ -1,1 +1,1 @@
-Javascript warm up!
+JAVASCRIPT

7494a7f7435167d8c448d368a62fba4b263807e9
Audraaires <sbansah360@gmail.com>
2023-04-29 12:42:12 +0000

    Javascript warm up

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+const argc = argv.length;
+
+if (argc === 0) {
+    console.log("No argument");
+} else if (argc === 1) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
